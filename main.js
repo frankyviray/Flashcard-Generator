65c6c823c7d5cc539f79a88d6a0d5dac4e94568a
frank yviray <frankyviray@yahoo.com>
2018-03-13 19:56:09 -0700

    done with basic card.js

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
+var inquirer = require ("inquirer");
 
-
-
-var firstPresident = new BasicCard {
-    "who was the first president of the United States?"
-    console.log(firstPresident.front);
-}
+var firstPresident = new BasicCard("who was the first president of the United States");
+console.log(firstPresident.front);
