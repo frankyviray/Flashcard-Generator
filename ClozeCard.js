c66eec7ce2c80611cfb2ce4b7fc25af36d24a3d8
frank yviray <frankyviray@yahoo.com>
2018-03-15 20:25:31 -0700

    final piece

---
+++ ClozeCard.js
@@ -0,0 +1,13 @@
+function ClozeCard(text, cloze) {
+    this.partial = text;
+    this.cloze = cloze;
+    this.fullText = function () {
+        var part2 = text.split(".").splice(3, 1)
+        var full = cloze + " " + part2
+        return full
+    }
+        ;
+
+
+}
+module.exports = ClozeCard;
--- main.js
+++ main.js
@@ -1,4 +1,47 @@
-var inquirer = require ("inquirer");
+var BasicCard = require("./BasicCard.js")
+var ClozeCard = require("./ClozeCard.js")
+var inquirer = require("inquirer")
 
-var firstPresident = new BasicCard("who was the first president of the United States");
-console.log(firstPresident.front);
+var president = new BasicCard(
+    "Who was the first president of the United States?", "George Washington");
+var presidentCloze = new ClozeCard(
+    "George Washington was the first president of the United States.");
+
+var theMVP = new BasicCard(
+    "Who has won the most NFL finals MVP awards?", "Tom Brady");
+var theMVPCloze = new ClozeCard(
+    "...has won the most NFL finals MVP awards.", "Tom Brady"
+)
+
+var questions = [president, presidentCloze, theMVP, theMVPCloze]
+
+var current;
+function chooseQuestion() {
+    var indexChoice = Math.floor(Math.random() * 16 + 1)
+    current = questions[indexChoice];
+    return current[Object.keys(current)[0]]
+}
+
+
+function ask() {
+    var choice = chooseQuestion();
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "userInput",
+            message: choice
+        }
+    ]).then(function (user) {
+        console.log(user.userInput)
+        if (user.userInput === current[Object.keys(current)[1]]) {
+            console.log("correct")
+            ask();
+        }
+        else {
+            console.log("the answer is" + current[Object.keys(current)[1]])
+            ask();
+        }
+    })
+}
+
+ask();
